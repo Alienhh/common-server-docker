fcfe570eff230a6aadfc06b34ade18208a480eab
huan.he5 <huan.he5@56qq.com>
2018-06-29 15:37:51 +0800

    add mysql v5.7.11

---
+++ run/run_mysql_v5.7.11.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#
+root_path=`cd "$(dirname "$0")"; pwd`
+
+compose_file=${root_path}/../conf/mysql/v5.7.11/docker-compose.yml
+
+# run docker
+docker-compose -f ${compose_file} up --build -d
